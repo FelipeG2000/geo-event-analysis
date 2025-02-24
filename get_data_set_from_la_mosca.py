0214677151b76dc43bfd112bda59b9b6836a08ad
Pipe <pipegarcia1918@hotmail.com>
2025-02-24 08:35:42 -0500

    feat: add monitoring and error handling for export tasks

--- get_data_set_from_la_mosca.py
+++ get_data_set_from_la_mosca.py
@@ -45,6 +45,7 @@ def get_landsat_data_set_from_la_mosca():
         image = landsat_collection_la_mosca.map(mask_landsat_8sr).mean().clip(roi_la_mosca).reproject(crs='EPSG:4326',
                                                                                                       scale=30)
         for band in image.bandNames().getInfo():
+            print(f"processing the follow band {band} of san carlos image in this dates {date[0]}-{date[1]}")
             description = f"Mean image of La Mosca captured between {date[0]} and {date[1]}, using the {band} band."
             task = ee.batch.Export.image.toDrive(
                 image=image.select(band),
@@ -57,7 +58,17 @@ def get_landsat_data_set_from_la_mosca():
                 maxPixels=1e13
             )
             task.start()
-            while task.active() and task.status().get('state') not in  ["FAILED", "CANCELLED"]:
+            while task.active():
+                status = task.status()
+                state = status.get('state')
+
+                if state == "FAILED":
+                    print(f"Error: {status.get('error_message')}")
+                    break
+                elif state == "CANCELLED":
+                    print("The export task was cancelled.")
+                    break
+
                 time.sleep(1)
 
 if __name__ == '__main__':
