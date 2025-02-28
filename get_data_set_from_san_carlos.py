f2b35fbb8c7bf3c616044a4a9685069f2d70e541
Pipe <pipegarcia1918@hotmail.com>
2025-02-27 21:37:05 -0500

    feat: creacte function to download visualized images from san carlos

--- get_data_set_from_san_carlos.py
+++ get_data_set_from_san_carlos.py
@@ -6,17 +6,14 @@ ee.Authenticate()
 ee.Initialize(project="investigation-project-pipe")
 
 
+POINTS_SAN_CARLOS = [[-75.0174891, 6.2002711],
+                    [-74.9691066, 6.2007604],
+                    [-74.9686103, 6.1675056],
+                    [-75.0172356, 6.1670877]]
+ROI_SAN_CARLOS = generate_roi_from_points(ee, POINTS_SAN_CARLOS)
+
 def get_landsat_data_set_from_san_carlos():
-    dates = [(str(AAAA) + MM1, str(AAAA) + MM2) for AAAA in range(2015, 2025)
-             for MM1, MM2 in zip(['-01-01', '-04-01', '-07-01', '-10-01'],
-                                 ['-03-31', '-06-30', '-09-30', '-12-31'])]
-
-    points_san_carlos = [[-75.0174891, 6.2002711],
-                         [-74.9691066, 6.2007604],
-                         [-74.9686103, 6.1675056],
-                         [-75.0172356, 6.1670877]]
-
-    roi_san_carlos = generate_roi_from_points(ee, points_san_carlos)
+    dates = generate_date_ranges(2015, 2025)
 
     collection_landsta_path = 'LANDSAT/LC08/C02/T1_L2'
     for date in dates:
@@ -25,8 +22,8 @@ def get_landsat_data_set_from_san_carlos():
                                                                collection_id=collection_landsta_path,
                                                                start=date[0],
                                                                end=date[1],
-                                                               roi=roi_san_carlos)
-        image = landsat_collection_san_carlos.map(mask_landsat_8sr).mean().clip(roi_san_carlos).reproject(crs='EPSG:4326',
+                                                               roi=ROI_SAN_CARLOS)
+        image = landsat_collection_san_carlos.map(mask_landsat_8sr).mean().clip(ROI_SAN_CARLOS).reproject(crs='EPSG:4326',
                                                                                                       scale=30)
         for band in image.bandNames().getInfo():
             print(f"processing the follow band {band} of san carlos image in this dates {date[0]}-{date[1]}")
@@ -36,7 +33,62 @@ def get_landsat_data_set_from_san_carlos():
                 description=description,
                 folder=band,
                 fileNamePrefix=f"San_carlos_mean_{date[0]}_{date[1]}",
-                region=roi_san_carlos.bounds().getInfo()['coordinates'],
+                region=ROI_SAN_CARLOS.bounds().getInfo()['coordinates'],
+                scale=30,
+                crs='EPSG:4326',
+                maxPixels=1e13
+            )
+            task.start()
+            while task.active():
+                status = task.status()
+                state = status.get('state')
+
+                if state == "FAILED":
+                    print(f"Error: {status.get('error_message')}")
+                    break
+                elif state == "CANCELLED":
+                    print("The export task was cancelled.")
+                    break
+
+                time.sleep(1)
+
+
+def get_landsat_visualisation_data_set_from_san_carlos():
+    """
+    Retrieves and exports mean Landsat images of the La Mosca region for multiple date ranges.
+
+    - Defines a region of interest (ROI) based on predefined coordinates.
+    - Extracts Landsat 8 imagery for each date range from 2015 to 2024.
+    - Computes the mean image for each period.
+    - Exports each band separately to Google Drive (organized by filename).
+
+    The exported files follow this naming convention:
+    - File name: `san_carlos_mean_{start_date}_{end_date}`
+    """
+
+    dates = generate_date_ranges(2019, 2025)
+
+    collection_landsta_path = 'LANDSAT/LC08/C02/T1_L2'
+
+    for date in dates:
+
+        landsat_collection_san_carlos = get_satellite_collection(ee_client=ee,
+                                                               collection_id=collection_landsta_path,
+                                                               start=date[0],
+                                                               end=date[1],
+                                                               roi=ROI_SAN_CARLOS)
+        image = landsat_collection_san_carlos.map(mask_landsat_8sr).mean().clip(ROI_SAN_CARLOS).reproject(crs='EPSG:4326',
+                                                                                                      scale=30)
+        for band in filter_landsat8_sr_st_bands(image.bandNames().getInfo()):
+            print(f"processing the follow band {band} of san carlos image in this dates {date[0]}-{date[1]}")
+            description = f"Mean image of San Carlos captured between {date[0]} and {date[1]}, using the {band} band."
+            visualization = get_landsat8_visualization_params(band)
+            task = ee.batch.Export.image.toDrive(
+                image=image.select(band).visualize(**visualization),
+                description=description,
+                folder=f'{band}',
+                fileNamePrefix=f"san_carlos_mean_{date[0]}_{date[1]}",
+                region=ROI_SAN_CARLOS.bounds().getInfo()['coordinates'],
                 scale=30,
                 crs='EPSG:4326',
                 maxPixels=1e13
@@ -57,4 +109,4 @@ def get_landsat_data_set_from_san_carlos():
 
 
 if __name__ == '__main__':
-    get_landsat_data_set_from_san_carlos()
+    get_landsat_visualisation_data_set_from_san_carlos()
