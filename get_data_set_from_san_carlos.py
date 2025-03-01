ddd82992be2ff8cc9b89bfab36aadb479c4a0d33
Pipe <pipegarcia1918@hotmail.com>
2025-03-01 10:00:59 -0500

    feat: add Sentinel-2 dataset export for San Carlos region
    -Implemented get_sentinel2_data_set_from_san_carlos to retrieve and export Sentinel-2 imagery.
    -Extracts mean images for multiple date ranges (2018-2024).
    -Clips and reprojects images to EPSG:4326 with a 10m scale.
    -Exports each band separately to Google Drive.
    -Integrated monitor_task for task status tracking.

--- get_data_set_from_san_carlos.py
+++ get_data_set_from_san_carlos.py
@@ -1,3 +1,4 @@
+from get_data_set_from_cocorna import ROI_COCORNA
 from utils import *
 import ee
 
@@ -43,7 +44,7 @@ def get_landsat_data_set_from_san_carlos():
 
 def get_landsat_visualisation_data_set_from_san_carlos():
     """
-    Retrieves and exports mean Landsat images of the La Mosca region for multiple date ranges.
+    Retrieves and exports mean Landsat images of the San Carlos region for multiple date ranges.
 
     - Defines a region of interest (ROI) based on predefined coordinates.
     - Extracts Landsat 8 imagery for each date range from 2015 to 2024.
@@ -85,5 +86,45 @@ def get_landsat_visualisation_data_set_from_san_carlos():
             monitor_task(task)
 
 
+def get_sentinel2_data_set_from_san_carlos():
+    """
+    Retrieves and exports mean Landsat images of the San Carlos region for multiple date ranges.
+
+    - Defines a region of interest (ROI) based on predefined coordinates.
+    - Extracts sentinel 2 imagery for each date range from 2018 to 2024.
+    - Computes the mean image for each period.
+    - Exports each band separately to Google Drive (organized by filename).
+
+    The exported files follow this naming convention:
+    - File name: `san_carlos_mean_{start_date}_{end_date}`
+    """
+
+    dates = generate_date_ranges(2018, 2025)
+
+    collection_sentinel2_path = "COPERNICUS/S2_SR_HARMONIZED"
+
+    for date in dates:
+
+        sentinel2_collection_san_carlos = get_satellite_collection(ee_client=ee, collection_id=collection_sentinel2_path,
+                                                                 start=date[0], end=date[1], roi=ROI_SAN_CARLOS)
+        image = sentinel2_collection_san_carlos.map(mask_sentinel2_sr).mean().clip(ROI_SAN_CARLOS).reproject(crs='EPSG:4326',
+                                                                                                         scale=10)
+        for band in image.bandNames().getInfo():
+            print(f"processing the follow band {band} of san carlos image in this dates {date[0]}-{date[1]}")
+            description = f"Mean image san carlos {date[0]} to {date[1]} using {band} band"
+            task = ee.batch.Export.image.toDrive(
+                image=image.select(band),
+                description=description,
+                folder=band,
+                fileNamePrefix=f"san_carlos_mean_{date[0]}_{date[1]}",
+                region=ROI_SAN_CARLOS.bounds().getInfo()['coordinates'],
+                scale=10,
+                crs='EPSG:4326',
+                maxPixels=1e13
+            )
+            task.start()
+            monitor_task(task)
+
+
 if __name__ == '__main__':
-    get_landsat_visualisation_data_set_from_san_carlos()
+    get_sentinel2_data_set_from_san_carlos()
