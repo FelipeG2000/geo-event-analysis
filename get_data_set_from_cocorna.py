55b265eb9d591f5ef730be7892a185a328cd86c0
Pipe <pipegarcia1918@hotmail.com>
2025-03-02 15:39:50 -0500

    feat: get sentinel2 visualized images for cocorna

--- get_data_set_from_cocorna.py
+++ get_data_set_from_cocorna.py
@@ -129,5 +129,45 @@ def get_sentinel2_data_set_from_cocorna():
             monitor_task(task)
 
 
+def get_sentinel2_visualized_data_set_from_cocorna():
+    """
+    Retrieves and exports mean Landsat images of the cocorna region for multiple date ranges.
+
+    - Defines a region of interest (ROI) based on predefined coordinates.
+    - Extracts sentinel 2 imagery for each date range from 2018 to 2024.
+    - Computes the mean image for each period.
+    - Exports each band separately to Google Drive (organized by filename).
+
+    The exported files follow this naming convention:
+    - File name: `cocorna_mean_{start_date}_{end_date}`
+    """
+
+    dates = generate_date_ranges(2023, 2025)
+
+    collection_sentinel2_path = "COPERNICUS/S2_SR_HARMONIZED"
+
+    for date in dates:
+
+        sentinel2_collection_cocorna = get_satellite_collection(ee_client=ee, collection_id=collection_sentinel2_path,
+                                                                   start=date[0], end=date[1], roi=ROI_COCORNA)
+        image = sentinel2_collection_cocorna.map(mask_sentinel2_sr).mean().clip(ROI_COCORNA).reproject(crs='EPSG:4326',
+                                                                                                         scale=10)
+        for band in filter_sentinel2_reflected_bands(image.bandNames().getInfo()):
+            print(f"processing the follow band {band} of cocorna image in this dates {date[0]}-{date[1]}")
+            description = f"Mean image cocorna {date[0]} to {date[1]} using {band} band"
+            task = ee.batch.Export.image.toDrive(
+                image=image.select(band).visualize(**{'min': 0, 'max': 1}),
+                description=description,
+                folder=band,
+                fileNamePrefix=f"cocorna_mean_{date[0]}_{date[1]}",
+                region=ROI_COCORNA.bounds().getInfo()['coordinates'],
+                scale=10,
+                crs='EPSG:4326',
+                maxPixels=1e13
+            )
+            task.start()
+            monitor_task(task)
+
+
 if __name__ == '__main__':
-    get_sentinel2_data_set_from_cocorna()
+    get_sentinel2_visualized_data_set_from_cocorna()
