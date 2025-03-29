5c9cf887f67e40304a8f3102b86e5d747f278a52
Pipe <pipegarcia1918@hotmail.com>
2025-03-29 13:49:26 -0500

    feat: first concept test to get index of an image

--- process_data/process_la_mosca.py
+++ process_data/process_la_mosca.py
@@ -1,7 +1,10 @@
-from utils import preserve_georef
 import rasterio
 import matplotlib.pyplot as plt
-BASEPATH = "/home/felipe/MiDrive/GEE_Exports/la_mosca"
+import numpy as np
+
+from process_data.process_images_tools import GeoImageProcessor
+
+BASEPATH = "/home/felipe/MiDrive/GEE_Exports/la_mosca/landsat8/bands"
 
 
 def create_ndvi_from_la_mosca():
@@ -16,8 +19,26 @@ def create_ndvi_from_la_mosca():
     plt.ylabel("Rows")
     plt.show()
 
+
+def calculate_index(band1, band2):
+    """Computes a normalized difference index like NDVI or NDWI."""
+    return (band1 - band2) / (band1 + band2 + 1e-6)  # Avoid division by zero
+
+
 if __name__ == "__main__":
-    create_ndvi_from_la_mosca()
+    image1_la_mosca = "/home/felipe/MiDrive/GEE_Exports/la_mosca/landsat8/bands/SR_B5/la_mosca_mean_2021-10-01_2021-12-31.tif"
+    image2_la_mosca = "/home/felipe/MiDrive/GEE_Exports/la_mosca/landsat8/bands/SR_B4/la_mosca_mean_2021-10-01_2021-12-31.tif"
+    geo_image = GeoImageProcessor(image1_la_mosca)
+    geo_red = GeoImageProcessor(image2_la_mosca)
+
+    ndvi = calculate_index(geo_image.data, geo_red.data)
+    geo_image.data = ndvi
+    plt.figure(figsize=(8, 6))
+    plt.imshow(ndvi, cmap="Reds")
+    plt.colorbar(label="Intensity")
+    plt.xlabel("Columns")
+    plt.ylabel("Rows")
+    plt.show()
 
 
 
