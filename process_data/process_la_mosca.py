dd73d68700da93e3d71b38c4d83d79b70ad95edb
Pipe <pipegarcia1918@hotmail.com>
2025-03-31 13:54:46 -0500

    feat: concept proof saving an image

--- process_data/process_la_mosca.py
+++ process_data/process_la_mosca.py
@@ -6,6 +6,14 @@ from process_data.process_images_tools import GeoImageProcessor
 
 BASEPATH = "/home/felipe/MiDrive/GEE_Exports/la_mosca/landsat8/bands"
 
+def scale_to_8bit(image):
+    """
+    Converts an image with values in range [-1, 1] to [0, 255] for visualization.
+    """
+    image = np.nan_to_num(image, nan=0, posinf=1, neginf=0)  # Remove NaNs again just in case
+    image = (image + 1) / 2  # Scale from [-1, 1] to [0, 1]
+    image = np.clip(image * 255, 0, 255).astype(np.uint8)  # Scale to [0, 255] safely
+    return image
 
 def create_ndvi_from_la_mosca():
     with rasterio.open(BASEPATH + '/sentinel1/descending/VH/la_mosca_first_find_2017-05-01_2017-05-31.tif') as src:
@@ -32,7 +40,8 @@ if __name__ == "__main__":
     geo_red = GeoImageProcessor(image2_la_mosca)
 
     ndvi = calculate_index(geo_image.data, geo_red.data)
-    geo_image.data = ndvi
+    geo_image.data = scale_to_8bit(ndvi)
+    geo_image.save("/home/felipe/MiDrive/concept_proof.tif")
     plt.figure(figsize=(8, 6))
     plt.imshow(ndvi, cmap="Reds")
     plt.colorbar(label="Intensity")
