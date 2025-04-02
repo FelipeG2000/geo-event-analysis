a307ab763a649790178a43b71cabb0321d821efe
Pipe <pipegarcia1918@hotmail.com>
2025-04-01 20:40:17 -0500

    fix: the method to save an image

--- process_data/process_images_tools.py
+++ process_data/process_images_tools.py
@@ -19,11 +19,13 @@ class GeoImageProcessor:
         self.image_path = image_path
         self.data, self.profile = self._load_image()
 
+
     def _load_image(self):
         """Loads the image and stores its metadata."""
         with rasterio.open(self.image_path) as src:
             profile = src.profile
             data = src.read(1).astype(np.float32)  # Load first band as float32
+            self.meta = src.meta
         return data, profile
 
     def apply(self, processing_function, *args, **kwargs):
@@ -42,7 +44,9 @@ class GeoImageProcessor:
         Args:
             output_path (str): Path to save the new image.
         """
-        self.profile.update(dtype=rasterio.float32)  # Ensure correct dtype
-        with rasterio.open(output_path, 'w', **self.profile) as dst:
+        self.meta.update(dtype=np.uint8, count=1)
+
+        with rasterio.open(output_path, 'w', **self.meta) as dst:
             dst.write(self.data, 1)
+            dst.close()
 
