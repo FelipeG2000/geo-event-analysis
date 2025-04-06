eb72eca04cb79ce559e745710501ffe9d252c342
Pipe <pipegarcia1918@hotmail.com>
2025-04-05 23:18:05 -0500

    feat: prcess ndvi images for la mosca

--- process_data/process_la_mosca.py
+++ process_data/process_la_mosca.py
@@ -7,8 +7,10 @@ from process_data.process_images_tools import GeoImageProcessor, calculate_index
 
 # Base path where Sentinel-2 images are stored
 BASEPATH_B3 = "/home/felipe/MiDrive/GEE_Exports/la_mosca/sentinel2/bands/B3"
+BASEPATH_B4 = "/home/felipe/MiDrive/GEE_Exports/la_mosca/sentinel2/bands/B4"
 BASEPATH_B8 = "/home/felipe/MiDrive/GEE_Exports/la_mosca/sentinel2/bands/B8"
 OUTPUT_DIR = "ndwi"
+NDVI_DIR = "ndwi"
 
 
 def get_ndwi_la_mosca():
@@ -35,6 +37,30 @@ def get_ndwi_la_mosca():
         print(f"Processed: {output_filename}")
 
 
+def get_ndvi_la_mosca():
+    """
+    Processes all Sentinel-2 images in the given directories to compute NDVI and save results.
+    """
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+    band4_files = sorted(glob.glob(os.path.join(BASEPATH_B4, "*.tif")))
+    band8_files = sorted(glob.glob(os.path.join(BASEPATH_B8, "*.tif")))
+
+    for b4_path, b8_path in zip(band4_files, band8_files):
+        filename = os.path.basename(b4_path)
+        output_filename = filename.replace("mean", "ndvi")
+        output_path = os.path.join(OUTPUT_DIR, output_filename)
+
+        geo_b4 = GeoImageProcessor(b4_path)
+        geo_b8 = GeoImageProcessor(b8_path)
+
+        ndwi = calculate_index(geo_b8.data, geo_b4.data)
+        geo_b4.data = scale_to_8bit(ndwi)
+        geo_b4.save(output_path)
+
+        print(f"Processed: {output_filename}")
+
+
 if __name__ == "__main__":
-    get_ndwi_la_mosca()
+    get_ndvi_la_mosca()
 
