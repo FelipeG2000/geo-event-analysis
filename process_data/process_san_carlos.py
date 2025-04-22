f0a0e207d017f16cfb0e7b759607b1b8a7168fa3
Pipe <pipegarcia1918@hotmail.com>
2025-04-21 23:42:10 -0500

    feat: process ndvi-landsat8 from san carlos

--- process_data/process_san_carlos.py
+++ process_data/process_san_carlos.py
@@ -58,6 +58,30 @@ def get_ndvi_san_carlos_sentinel2():
         print(f"Processed: {output_filename}")
 
 
+def get_ndvi_san_carlos_landsat8():
+    """
+    Processes all Landsat 8 images in the given directories to compute NDVI and save results.
+    """
+    os.makedirs(NDVI_DIR, exist_ok=True)
+
+    band4_files = sorted(glob.glob(os.path.join(BASEPATH_LANDSAT8 + 'SR_B4', "*.tif")))
+    band5_files = sorted(glob.glob(os.path.join(BASEPATH_LANDSAT8 + 'SR_B5', "*.tif")))
+
+    for b4_path, b5_path in zip(band4_files, band5_files):
+        filename = os.path.basename(b4_path)
+        output_filename = filename.replace("mean", "ndvi")
+        output_path = os.path.join(NDVI_DIR, output_filename)
+
+        geo_b4 = GeoImageProcessor(b4_path)
+        geo_b5 = GeoImageProcessor(b5_path)
+
+        ndvi = calculate_index(geo_b5.data, geo_b4.data)  # (NIR - Red) / (NIR + Red)
+        geo_b4.data = scale_to_8bit(ndvi)
+        geo_b4.save(output_path)
+
+        print(f"Processed Landsat 8 NDVI: {output_filename}")
+
+
 if __name__ == "__main__":
-    get_ndvi_san_carlos_sentinel2()
+    get_ndvi_san_carlos_landsat8()
 
